90f16decbaad4cd2dbcb6b289d42c6ebf7000c05
DuanJiaNing <aimeimeits@gmail.com>
2019-11-05 13:50:32 +0800

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="test">{{test}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: "abc"
+            }
+        },
+        props: {},
+    };
+</script>
+
+<style>
+    #test {
+        color: red;
+    }
+</style>
